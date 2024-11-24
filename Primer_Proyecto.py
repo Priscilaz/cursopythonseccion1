cf1d1edc3241c2dafe20e875efc5a0d0b86b1d94
Priscilaz <priscila.zuniga@udla.edu.ec>
2024-11-23 19:51:42 -0500

    Primer commit

---
+++ Primer_Proyecto.py
@@ -0,0 +1,24 @@
+#Seccion 1
+
+#print("Holaa Mundo")
+
+#print("Holaa Mundo 'Hola'")
+
+#print(100+8)
+
+#print("Hola" + ' Priscila')
+
+#print("Hola soy  \"Marilyn Cote\"")
+#print("Linea 1 \nLinea 2")
+#print("\tLetra A")
+#print("This isn\'t a number")
+#print("This is a Back slash \\")
+#input("Ingresa")
+#print(input("Dime tu nombre: "))
+#print("Tu eleccion fue: " + input("Ingresa un sabor de helado: "))
+#print("Tu nombre es: " + input("Dime tu Nombre: ")
+      #+ " " + input("Dime tu Apellido: "))
+
+##Proyecto dia 1
+print("Tu cerveza \nse llama: " + input("Ingresa un color: ") + " "
+      + input("Ingresa una ciudad: ") + '\nFelicidades!!')
